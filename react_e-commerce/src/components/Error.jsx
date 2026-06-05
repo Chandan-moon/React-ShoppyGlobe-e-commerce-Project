624195e1b31b1c1f1ce8d46141622c3b00b5a1fb
chandan-moon <testid11.com@gmail.com>
2026-06-05 17:21:33 +0530

    Router Error handling component created

--- react_e-commerce/src/components/Error.jsx
+++ react_e-commerce/src/components/Error.jsx
@@ -1,11 +1,25 @@
 
 
+import { useRouteError } from "react-router-dom";
+
+
+
 function Error() {
+
+    const err = useRouteError();
 
 
     return (
 
-        <></>
+        <div>
+
+            <h1>Oops !!</h1>
+            <h2>Please Enter Correct Path</h2>
+
+            <h3>{err.status} {err.statusText}</h3>
+            <h3>{err.data}</h3>
+
+        </div>
     )
 }
 
